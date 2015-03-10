0a39e6509c1983d53584b0d35a88d7c53b48750d
Telasco Communications <telascocommunications@Telascos-MacBook-Air.local>
2015-03-10 15:55:44 +0400

    adding logs

--- web-service/module/Roaming/src/Roaming/Controller/AbstractBaseController.php
+++ web-service/module/Roaming/src/Roaming/Controller/AbstractBaseController.php
@@ -26,7 +26,7 @@ class AbstractBaseController extends \Zend\Mvc\Controller\AbstractActionControll
     protected $logger;
     
     public function __construct() {
-        $writer = new \Zend\Log\Writer\Stream('../logs/app.log');
+        $writer = new \Zend\Log\Writer\Stream('app.log');
         $this->logger = new \Zend\Log\Logger;
         $this->logger->addWriter($writer);
     }
